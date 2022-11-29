f980172da24b376c2553d46143ad43029b1594b3
KERRCAM <90578826+KERRCAM@users.noreply.github.com>
2022-11-29 19:41:30 +0000

    Update contactDeath.cs

--- contactDeath.cs
+++ contactDeath.cs
@@ -4,11 +4,26 @@ public class contactDeath : MonoBehaviour
 {
     public GameObject start;
     public GameObject Player;
+    playerMovement playerMovement;
+    private Vector2 respawnPos;
+
+    private void Awake()
+    {
+        playerMovement = Player.GetComponent<playerMovement>();
+    }
+
+    private void Update()
+    {
+        respawnPos = playerMovement.respawnPoint;
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-            Player.transform.position = start.transform.position;
+        
+        if (other.gameObject.CompareTag("Player") )
+        {
+            Player.transform.position = respawnPos;
+        }
     }
 
 }
