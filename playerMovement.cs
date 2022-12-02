e02396e3e8e518ce2fb23f583d398302275b6239
KERRCAM <90578826+KERRCAM@users.noreply.github.com>
2022-12-02 18:42:10 +0000

    Update playerMovement.cs

--- playerMovement.cs
+++ playerMovement.cs
@@ -7,6 +7,13 @@ public class playerMovement : MonoBehaviour
     private Rigidbody2D body;
     [SerializeField] private bool inAir;
     public Vector2 respawnPoint;
+    [SerializeField] private bool rightArrowAvailable = false;
+    [SerializeField] private bool leftArrowAvailable = false;
+    [SerializeField] private bool upArrowAvailable = false;
+    [SerializeField] private bool wAvailable = false;
+    [SerializeField] private bool aAvailable = false;
+    [SerializeField] private bool dAvailable = false;
+    
 
     private void Awake()
     {
@@ -16,41 +23,75 @@ public class playerMovement : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * Xspeed, body.velocity.y);
+        body.velocity = new Vector2(0 * Xspeed, body.velocity.y);
+        if (Input.GetKey(KeyCode.D) && dAvailable == true || Input.GetKey(KeyCode.RightArrow) && rightArrowAvailable == true)
+        {
+            body.velocity = new Vector2(1 * Xspeed, body.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.A) && aAvailable == true || Input.GetKey(KeyCode.LeftArrow) && leftArrowAvailable == true)
+        {
+            body.velocity = new Vector2(-1 * Xspeed, body.velocity.y);
+        }
 
-        if (Input.GetKey(KeyCode.Space) && inAir == false)
+        if (Input.GetKey(KeyCode.W) && inAir == false && wAvailable == true || Input.GetKey(KeyCode.UpArrow) && inAir == false && upArrowAvailable == true)
+        {
             body.velocity = new Vector2(body.velocity.x, Yspeed);
+        }
+     
+
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("ground"))
+        {
             inAir = false;
+        }
     }
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("ground"))
+        {
             inAir = true;
-    } 
+        }
+    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.tag == "checkpoint")
+        if (other.gameObject.CompareTag("checkpoint"))
         {
             respawnPoint = transform.position;
+        }
+        if (other.gameObject.CompareTag("ground"))
+        {
+            inAir = false;
+        }
+        if (other.gameObject.CompareTag("P1UP2LR"))
+        {
+            wAvailable = true;
+            leftArrowAvailable = true;
+            rightArrowAvailable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            inAir = true;
+        }
+        if (other.gameObject.CompareTag("P1UP2LR"))
+        {
+            wAvailable = false;
+            leftArrowAvailable = false;
+            rightArrowAvailable = false;
         }
     }
 
     
-    private void OnTriggerEnter(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("ground"))
-            inAir = false;
-    }
-    private void OnTriggerExit(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("ground"))
-            inAir = true;
-    }
+
     
 }
